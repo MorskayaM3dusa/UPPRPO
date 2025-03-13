50fe6340ff20466ba6a2bb36c94547edf8102991
Roman <tsaregorodtsev.roma05@gmail.com>
2025-03-13 12:58:42 +0700

    add task 2.2

--- main.cpp
+++ main.cpp
@@ -20,6 +20,24 @@ TEST(CandleTest, BodyContainsEqual) {
   EXPECT_FALSE(candle.body_contains(1.1));
 }
 
+TEST(CandleTest, ContainsWithinRange) {
+  Candle candle(1.0, 2.0, 0.5, 1.5);
+  EXPECT_TRUE(candle.contains(1.2));
+  EXPECT_FALSE(candle.contains(2.1));
+}
+
+TEST(CandleTest, ContainsBoundary) {
+  Candle candle(1.0, 2.0, 0.5, 1.5);
+  EXPECT_TRUE(candle.contains(2.0));
+  EXPECT_FALSE(candle.contains(0.4));
+}
+
+TEST(CandleTest, ContainsOutsideRange) {
+  Candle candle(1.0, 2.0, 0.5, 1.5);
+  EXPECT_FALSE(candle.contains(2.5));
+  EXPECT_FALSE(candle.contains(-0.5));
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
